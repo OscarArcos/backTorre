8a3e6ad1df69746f189a1cd72cf72aa684a0bbfe
E5cu3l4DI* <escueladigitalcarvajal@gmail.com>
2021-01-15 02:00:24 -0500

    no cors

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "GET, POST");
     return res.status(200).json({});
