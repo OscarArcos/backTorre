c82605fcd7f6b697a08bbeef0e5731b9c2d8b953
E5cu3l4DI* <escueladigitalcarvajal@gmail.com>
2021-01-14 22:48:31 -0500

    no cors

--- src/index.js
+++ src/index.js
@@ -12,10 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "GET, POST");
     return res.status(200).json({});
